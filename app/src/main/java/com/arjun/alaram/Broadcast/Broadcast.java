02644d0de18081d937b3b396f681667e198d38a2
Arjun Adhikary <reasonworld12345@gmail.com>
2020-09-23 15:07:51 +0545

    v0.0.1

--- app/src/main/java/com/arjun/alaram/Broadcast/Broadcast.java
+++ app/src/main/java/com/arjun/alaram/Broadcast/Broadcast.java
@@ -1,14 +1,10 @@
 package com.arjun.alaram.Broadcast;
 
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import static com.arjun.alaram.Broadcast.NC.channelID;
-import android.util.Log;
+
 
 import com.arjun.alaram.MainActivity;
 
